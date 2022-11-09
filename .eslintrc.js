9782920d865a7aa908e88980ddb20f41967ad04f
jpiazzal <joel.piazzalungalerat@gmail.com>
2022-11-09 16:56:35 +0100

    feat(linter): Add Eslint / Prettier configuration and editorconfig file

---
+++ .eslintrc.js
@@ -0,0 +1,76 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:github/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:sonarjs/recommended',
+        'plugin:unicorn/recommended',
+        'prettier',
+        'react-app'
+    ],
+    plugins: ['@typescript-eslint', 'github', 'jsx-a11y', 'prettier', 'sonarjs'],
+    rules: {
+        // OFF
+        'eslint-comments/no-unused-disable': 'off',
+        'filenames/match-regex': 'off',
+        'import/no-unresolved': 'off',
+        'unicorn/no-null': 'off',
+        // ERRORS
+        '@typescript-eslint/no-unused-vars': 'error',
+        'jsx-a11y/aria-role': [
+            'error',
+            {
+                allowedInvalidRoles: ['text'],
+                ignoreNonDOM: true
+            }
+        ],
+        'import/order': [
+            'error',
+            {
+                groups: [
+                    'builtin', // Node "builtin" modules, eg: import path from "path";
+                    'external', // External modules, eg: import moment from "moment";
+                    'internal', // Internal modules (absolute imports), eg: import {apiService} from "app/services/core";
+                    ['sibling', 'parent', 'index'], // Relative imports, eg: import foo from "../foo"; / import bar from "./bar"; / import main from "./";
+                    'object', // "object"-imports (TypeScript only), eg: import log = console.log;
+                    'type' // "type" imports (TypeScript only), eg: import type { Foo } from "foo";
+                ],
+                'newlines-between': 'always',
+                alphabetize: {
+                    order: 'asc',
+                    caseInsensitive: true
+                }
+            }
+        ],
+        'no-console': 'error',
+        'no-restricted-imports': [
+            'error',
+            {
+                patterns: ['@mui/*/*/*', '!@mui/material/test-utils/*']
+            }
+        ],
+        'prettier/prettier': 'error',
+        'unicorn/filename-case': [
+            'error',
+            {
+                cases: {
+                    camelCase: true,
+                    pascalCase: true
+                }
+            }
+        ],
+        'unicorn/prevent-abbreviations': [
+            'error',
+            {
+                replacements: {
+                    props: {
+                        properties: false
+                    }
+                }
+            }
+        ]
+    }
+};
