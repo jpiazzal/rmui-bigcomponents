66973664133b9d5f5d749be64eede99ea1d92f8b
jpiazzal <joel.piazzalungalerat@gmail.com>
2022-11-14 17:05:42 +0100

    style(footer): Remove position fixed of the footer

--- packages/rmui-bigcomponents/src/components/Footer/Footer.tsx
+++ packages/rmui-bigcomponents/src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ interface FooterProps {
 
 export default function Footer(props: FooterProps) {
     return (
-        <footer style={{ position: 'fixed', bottom: 0, width: '100%', padding: '1.25rem' }} role="contentinfo">
+        <footer style={{ padding: '1.25rem' }} role="contentinfo">
             <Copyright text={props.copyrightText} />
         </footer>
     );
