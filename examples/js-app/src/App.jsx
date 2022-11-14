53e921c8b02e7d9bcb8943a9135a17b9d72c6489
jpiazzal <joel.piazzalungalerat@gmail.com>
2022-11-14 11:56:06 +0100

    example(js-app): Add js app using react and rmui-bigcomponents

---
+++ examples/js-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { Footer } from 'rmui-bigcomponents';
+
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>RMUI Big Components - Js App</p>
+            </header>
+            <Footer copyrightText="All right reserved" />
+        </div>
+    );
+}
+
+export default App;
