309f722b3ed645420398ef2f58bc45c06056b6df
jpiazzal <joel.piazzalungalerat@gmail.com>
2022-11-15 10:58:40 +0100

    feat(footer): socialMediaLinks and copyrightText props are now optional

--- packages/rmui-bigcomponents/src/components/Footer/Footer.tsx
+++ packages/rmui-bigcomponents/src/components/Footer/Footer.tsx
@@ -2,15 +2,16 @@ import Copyright from './Copyright';
 import SocialMediaLinks from './SocialMediaLinks';
 
 interface FooterProps {
-    socialMediaLinks: Array<{ name: string; href: string; title?: string }>;
-    copyrightText: string;
+    socialMediaLinks?: Array<{ name: string; href: string; title?: string }>;
+    copyrightText?: string;
 }
 
 export default function Footer(props: FooterProps) {
+    const { socialMediaLinks, copyrightText } = props;
     return (
         <footer style={{ padding: '1rem' }} role="contentinfo">
-            <SocialMediaLinks socialMedias={props.socialMediaLinks} />
-            <Copyright text={props.copyrightText} />
+            {socialMediaLinks && <SocialMediaLinks socialMedias={socialMediaLinks} />}
+            {copyrightText && <Copyright text={copyrightText} />}
         </footer>
     );
 }
